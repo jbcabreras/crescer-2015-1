986b7f00c2bf1f8e00d2a530a098fb17990ad2b8
Rodrigo Frohlich <rodrigofcb@gmail.com>
2015-04-15 12:14:56 -0300

    adicionado metodos na classe ExercitoDeElfos, taVazi(), tamanho(), getStatus(), retornaElfosComStatus() e seus respectivos testes

--- src/modulo-01-java-OO/Dia01/exercitoDeElfos.java
+++ src/modulo-01-java-OO/Dia01/exercitoDeElfos.java
@@ -6,10 +6,12 @@ import java.util.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class exercitoDeElfos
+public class ExercitoDeElfos
 {
-    private HashMap<String, Elfo> exercito = new HashMap<>();
     
+    private HashMap<String, Elfo> exercito = new HashMap<>();
+    private Status statusExercito = Status.VIVO;
+      
     public void alistarElfo(Elfo umElfo){
         if(umElfo instanceof ElfoVerde || umElfo instanceof ElfoNoturno){
             exercito.put(umElfo.getNome(), umElfo);
@@ -18,5 +20,32 @@ public class exercitoDeElfos
     
     public Elfo buscarElfo(String nomeElfo){
         return exercito.get(nomeElfo);
+    }
+    
+    public boolean taVazi(){
+        return exercito.isEmpty();
+    }
+    
+    public int tamanho(){
+        return exercito.size();
+    }
+    
+    public Status getStatus(){
+        return this.statusExercito;
+    }
+    
+    public ArrayList<Elfo> retornaElfosComStatus(Status umStatus){
+        Elfo umElfo;
+        ArrayList<Elfo> elfosComMesmoStatus = new ArrayList<>();
+                
+        for(String chave : exercito.keySet()){
+            umElfo = exercito.get(chave);            
+            
+            if(umElfo.getStatus() == umStatus){
+                elfosComMesmoStatus.add(umElfo);
+            }
+        }
+        
+        return elfosComMesmoStatus; 
     }
 }
--- src/modulo-01-java-OO/Dia01/exercitoDeElfosTest.java
+++ src/modulo-01-java-OO/Dia01/exercitoDeElfosTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.*;
 
 /**
  * The test class exercitoDeElfosTest.
@@ -11,12 +12,100 @@ import org.junit.Test;
  * @author  (your name)
  * @version (a version number or a date)
  */
-public class exercitoDeElfosTest
+public class ExercitoDeElfosTest
 {
     @Test
     public void AlistaElfoNoExercitoComNenhumElfo(){
-        private HashMap<String, Elfo> exercitoEsperado = new HashMap<>();
+        HashMap<String, Elfo> eEsperado = new HashMap<>();
         
+        ExercitoDeElfos eObtido = new ExercitoDeElfos();
         
+        assertEquals(eEsperado.isEmpty(), eObtido.taVazi());        
+    }
+    
+     @Test
+    public void AlistaElfoNoExercitoComUmElfoGenerico(){
+        HashMap<String, Elfo> eEsperado = new HashMap<>();
+        Elfo e = new Elfo("teste");
+        
+        ExercitoDeElfos eObtido = new ExercitoDeElfos();
+        eObtido.alistarElfo(e);
+        
+        assertEquals(eEsperado.isEmpty(), eObtido.taVazi());        
+    }
+    
+    
+     @Test
+    public void AlistaElfoNoExercitoComUmElfoNoturno(){
+        HashMap<String, Elfo> eEsperado = new HashMap<>();
+        ElfoNoturno en = new ElfoNoturno("teste");
+        
+        eEsperado.put("teste", en);
+        
+        ExercitoDeElfos eObtido = new ExercitoDeElfos();
+        eObtido.alistarElfo(en);
+        
+        assertEquals(eEsperado.get("teste"), eObtido.buscarElfo("teste"));        
+    }
+    
+    @Test
+    public void AlistaElfoNoExercitoComUmElfoVerde(){
+        HashMap<String, Elfo> eEsperado = new HashMap<>();
+        ElfoVerde ev = new ElfoVerde("teste");
+        
+        eEsperado.put("teste", ev);
+        
+        ExercitoDeElfos eObtido = new ExercitoDeElfos();
+        eObtido.alistarElfo(ev);
+        
+        assertEquals(eEsperado.get("teste"), eObtido.buscarElfo("teste"));        
+    }
+    
+        @Test
+    public void AlistaElfoNoExercitoCom5ElfosSortidos(){
+        HashMap<String, Elfo> eEsperado = new HashMap<>();
+        ElfoVerde ev1 = new ElfoVerde("teste1");
+        ElfoVerde ev2 = new ElfoVerde("teste2");
+        ElfoNoturno en1 = new ElfoNoturno("teste3");
+        ElfoNoturno en2 = new ElfoNoturno("teste4");
+        Elfo e = new Elfo("e");
+        
+        eEsperado.put("teste1", ev1);
+        eEsperado.put("teste2", ev2);
+        eEsperado.put("teste3", en1);
+        eEsperado.put("teste4", en2);
+        
+        ExercitoDeElfos eObtido = new ExercitoDeElfos();
+        eObtido.alistarElfo(ev1);
+        eObtido.alistarElfo(ev2);
+        eObtido.alistarElfo(en1);
+        eObtido.alistarElfo(en2);
+        eObtido.alistarElfo(e);
+        
+        assertEquals(eEsperado.size(), eObtido.tamanho());        
+    }
+    
+    @Test
+    public void RetornaElfosComMesmoStatus(){
+        HashMap<String, Elfo> eEsperado = new HashMap<>();
+        ElfoVerde ev1 = new ElfoVerde("teste1");
+        ElfoVerde ev2 = new ElfoVerde("teste2");
+        ElfoNoturno en1 = new ElfoNoturno("teste3");
+        ElfoNoturno en2 = new ElfoNoturno("teste4");
+        Elfo e = new Elfo("e");
+        
+        eEsperado.put("teste1", ev1);
+        eEsperado.put("teste2", ev2);
+        eEsperado.put("teste3", en1);
+        eEsperado.put("teste4", en2);
+        
+        ExercitoDeElfos eObtido = new ExercitoDeElfos();
+        eObtido.alistarElfo(ev1);
+        eObtido.alistarElfo(ev2);
+        eObtido.alistarElfo(en1);
+        eObtido.alistarElfo(en2);
+        eObtido.alistarElfo(e);
+        
+        assertEquals(eEsperado.values(), eObtido.retornaElfosComStatus(Status.VIVO));        
     }
 }
