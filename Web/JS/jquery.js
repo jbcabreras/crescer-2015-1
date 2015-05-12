46a27252f67dda00a0c663cf06aed075ca9c6e3b
Rodrigo Frohlich <rodrigofcb@gmail.com>
2015-05-12 13:54:08 -0300

    update dos exercícios

--- Web/JS/jquery.js
+++ Web/JS/jquery.js
@@ -85,3 +85,67 @@ Não utilize os pseudo-seletores :eq, :gt ou lt!
 */
 
 $("#myList li[class='current bar']").next().text().length + $("#myList li[class='current bar']:last").text().length
+
+/*
+2A
+
+Faça uma instrução jQuery que selecione todos elementos img da página que possuem alt e os 
+imprima no log do console.
+*/
+
+console.log($("img[alt]"));
+
+/*
+2B
+
+Faça uma instrução jQuery que selecione o input text pelo atributo, depois navegue até seu elemento
+ pai e adicione a classe template nele.
+
+:warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning:
+
+Não utilize pseudo-seletores e não selecione diretamente o pai do input text.
+*/
+
+$("input[type='text']").parent().addClass('template')
+
+/*
+2C
+
+Faça uma instrução jQuery que selecione o item de lista dentro de #myList que possui uma 
+classe "current" e:
+Remova esta classe dele.
+Adicione uma classe "current" no próximo item de lista.
+:warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning:
+
+Faça tudo isso em apenas um encadeamento de chamadas. Ex:
+
+$('select from html where...').funcao1().funcao2().funcao3().funcao4().funcao5();
+*/
+
+$("#myList li[class='current bar']").removeClass('current bar').next().addClass('current bar')
+
+/*
+2D
+
+Faça uma instrução jQuery que selecione o elemento h2 dentro de #specials, altere seu texto para "Promoções", depois atravesse e altere o texto da opção "Friday" para "Dimitri".
+
+:warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning:
+
+Não utilize pseudo-seletores.
+Faça tudo isso em apenas um encadeamento de chamadas. Ex:
+$('select from html where...').funcao1().funcao2().funcao3().funcao4().funcao5();
+*/
+
+
+
+/*
+2E
+
+Faça uma instrução jQuery que selecione o primeiro item de lista no elemento #slideshow; adicione a classe "current" a ele e então adicione a classe "disabled" para seus elementos sibling.
+
+:warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning::warning:
+
+Não utilize pseudo-seletores.
+Faça tudo isso em apenas um encadeamento de chamadas. Ex:
+$('select from html where...').funcao1().funcao2().funcao3().funcao4().funcao5();
+*/
